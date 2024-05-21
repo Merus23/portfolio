1e4c431a4c2ee678e7cc8a91297754cf491d3a72
Merus23 <mateus.satira@gmail.com>
2024-05-20 23:17:43 -0300

    refactor: adiciona props ao component de navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
+  const items = [
+    { title: "Team", url: "/about" },
+    { title: "Sobre", url: "/about" },
+  ];
   return (
     <>
-      <Navbar />
+      <Navbar logo={{ title: "home", url: "/" }} items={items} />
       <main></main>
     </>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,63 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import { StaticImageData } from "next/image";
 
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
-type Props = {};
+import { usePathname } from "next/navigation";
 
-export default function Navbar({}: Props) {
+interface item {
+  title: string;
+  url: string;
+}
+
+type Props = {
+  logo: StaticImageData | item;
+  items: item[];
+  itemExtraClasses?: string;
+};
+
+/**
+ *
+ * @param param0
+ * @returns
+ */
+export default function Navbar({ logo, items, itemExtraClasses }: Props) {
+  const currentPath = usePathname();
+
   return (
-    <nav className={GeistSans.className}>
-      <ul className="flex justify-between font-medium text-xl">
-        <li className="px-8 py-4 ">
-          <Link
-            href="/"
-            className="hover:border-b-2 hover:border-black transition ease-in-out duration-300 "
-          >
-            LUMA Dev
-          </Link>
+    <nav
+      className={
+        GeistSans.className +
+        " flex justify-between items-center font-medium text-xl"
+      }
+    >
+      <ul>
+        <li className=" py-4 px-12">
+          <Link href="/">LUMA Dev</Link>
         </li>
-        <li className="px-8 py-4">
-          <Link
-            href="/about"
-            className="hover:border-b-2 hover:border-black transition ease-in-out duration-300"
-          >
-            Sobre
-          </Link>
-        </li>
+      </ul>
+      {/* 
+      Follow the documentation:
+      https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#link-component
+       */}
+      <ul className="flex justify-around">
+        {items.map((item) => (
+          <li className="py-4 px-12">
+            <Link
+              href={item.url}
+              className={
+                (itemExtraClasses ? itemExtraClasses : "") +
+                " hover:border-b-2 hover:border-black transition ease-in-out duration-300"
+              }
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
