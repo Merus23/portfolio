ca944dbcd26bdf35b7a5ef6dce52f1885bcc3b10
marcospaulor <dev.silva.marcos@gmail.com>
2024-05-25 22:11:31 -0300

    refactor: Optimize WorkGallery component and add MainText component to improve homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
+'use client';
 import WorkGallery from '@/components/WorkGallery/WorkGallery';
+import MainText from '@/components/MainText/MainText';
+
+const worksData = [
+  {
+    href: 'work/days/',
+    src: '/images/days.webp',
+    company: 'Days',
+    description: 'iOS Countdown App',
+  },
+  {
+    href: 'work/tripscout/',
+    src: '/images/tripscout.webp',
+    company: 'Tripscout',
+    description: 'Travel Planner',
+  },
+  {
+    href: 'work/athotel/',
+    src: '/images/athotel.webp',
+    company: '@hotel',
+    description: 'Travel booking platform',
+  },
+  {
+    href: 'work/invision/',
+    src: '/images/invision.webp',
+    company: 'InVision',
+    description: 'Digital product design platform',
+  },
+  {
+    href: 'work/archive/',
+    src: '/images/archive.webp',
+    company: 'Archive',
+    description: 'Past design adventures',
+  },
+];
 
 export default function Home() {
-  
   return (
     <>
       <main>
-        <WorkGallery />
+        <MainText text="Product Design. Interfaces. Systems. Strategy. UX. & More" />
+        <WorkGallery works={worksData} />
       </main>
     </>
   );
