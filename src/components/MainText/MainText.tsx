b410f9de78b5ef6f750463a583bccc09b89af0c2
marcospaulor <dev.silva.marcos@gmail.com>
2024-05-25 22:11:07 -0300

    feat: Add MainText component with fade-in animation on scroll

---
+++ src/components/MainText/MainText.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { motion } from 'framer-motion';
+
+// Definindo a interface para os props do componente
+interface MainTextProps {
+  text: string;
+}
+
+/**
+ * @description The MainText component displays a heading with a fade-in animation on scroll
+ * @param text - The text to be displayed inside the heading
+ * @returns The MainText component
+ */
+export default function MainText({ text }: MainTextProps) {
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <motion.div
+      className="h-480 w-full max-w-7xl flex justify-center items-center"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, delay: 0.1 }}
+      variants={itemVariants}
+    >
+      <h1 className="w-full md:text-7xl text-5xl text-wrap font-semibold leading-loose">
+        {text}
+      </h1>
+    </motion.div>
+  );
+}
