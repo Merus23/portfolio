ac41b40490ce6794bee90f2d078d10a6691bac8a
Merus23 <mateus.satira@gmail.com>
2024-05-27 21:34:29 -0300

    feat: add Team component

---
+++ src/components/Team/Team.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+type Props = {};
+
+export default function Team({}: Props) {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
