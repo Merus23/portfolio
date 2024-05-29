d25db30b4d3ffb84894859c794bd9d465ffe07f1
Merus23 <mateus.satira@gmail.com>
2024-05-28 23:05:39 -0300

    feat: call the Card component at the Team Component

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -5,8 +5,17 @@ type Props = {};
 
 export default function Team({}: Props) {
   return (
-    <div>
-      <Card />
+    <div className="mt-36">
+      <h2 className="font-medium text-3xl md:text-4xl lg:text-5xl leading-tight mt-14 mb-16">
+        {`Our Team`}
+      </h2>
+      <div className="grid grid-cols-3 grid-rows-2 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
