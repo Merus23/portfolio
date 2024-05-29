6f21d7d113e426aed58df0bac568fdacec614c76
Merus23 <mateus.satira@gmail.com>
2024-05-28 23:04:37 -0300

    feat: finish the card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type Props = {};
+type Props = {
+  css?: string;
+};
 
-export default function Card({}: Props) {
+export default function Card({ css }: Props) {
   return (
-    <div
-      className={`w-full max-w-72 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700`}
-    >
+    <div className={css + `w-full max-w-72 bg-white rounded-lg shadow-xl`}>
       <div className="flex flex-col items-center pb-6 pt-4">
         <Image
           className="w-24 h-24 mb-3 rounded-full shadow-lg"
@@ -23,13 +23,30 @@ export default function Card({}: Props) {
         <span className="text-sm text-gray-500 dark:text-gray-400">
           Visual Designer
         </span>
-        <div className="flex mt-4 md:mt-6">
+        <div className="flex mt-4 md:mt-6 gap-3">
           <Link
             target="_blank"
             href={`https://www.linkedin.com/in/mateus-silva-893a4322b/`}
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-950 dark:focus:ring-blue-300"
+            //className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-950 dark:focus:ring-blue-300"
+            className="max-w-8 hover:scale-110"
           >
-            Linkedin
+            <img src="/images/social_media_icons/linkedin.svg" alt="" />
+          </Link>
+          <Link
+            target="_blank"
+            href={`https://www.linkedin.com/in/mateus-silva-893a4322b/`}
+            //className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-950 dark:focus:ring-blue-300"
+            className="max-w-8 hover:scale-110"
+          >
+            <img src="/images/social_media_icons/github.svg" alt="" />
+          </Link>
+          <Link
+            target="_blank"
+            href={`https://www.linkedin.com/in/mateus-silva-893a4322b/`}
+            //className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-950 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-700 dark:hover:bg-gray-950 dark:focus:ring-blue-300"
+            className="max-w-8 hover:scale-110"
+          >
+            <img src="/images/social_media_icons/linkedin.svg" alt="" />
           </Link>
         </div>
       </div>
