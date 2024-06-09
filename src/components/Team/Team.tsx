93e14e881f1fbec60b175189c4dee8679c5c1969
Merus23 <mateus.satira@gmail.com>
2024-06-09 12:15:17 -0300

    refactor: change the cards presentation using map method

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,22 +1,125 @@
 import React from 'react';
-import Card from '../Card/Card';
+import Card, { Member } from '../Card/Card';
 
 type Props = {};
 
 export default function Team({}: Props) {
+  const members: Member[] = [
+    {
+      name: 'Bonnie Green',
+      role: 'Visual Designer',
+      image: '/images/team/main.jpg',
+      networks: [
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/linkedin.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/github.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/twitter.svg',
+        },
+      ],
+    },
+    {
+      name: 'Bonnie Green',
+      role: 'Visual Designer',
+      image: '/images/team/main.jpg',
+      networks: [
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/linkedin.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/github.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/twitter.svg',
+        },
+      ],
+    },
+    {
+      name: 'Bonnie Green',
+      role: 'Visual Designer',
+      image: '/images/team/main.jpg',
+      networks: [
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/linkedin.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/github.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/twitter.svg',
+        },
+      ],
+    },
+    {
+      name: 'Bonnie Green',
+      role: 'Visual Designer',
+      image: '/images/team/main.jpg',
+      networks: [
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/linkedin.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/github.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/twitter.svg',
+        },
+      ],
+    },
+    {
+      name: 'Bonnie Green',
+      role: 'Visual Designer',
+      image: '/images/team/main.jpg',
+      networks: [
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/linkedin.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/github.svg',
+        },
+        {
+          link: 'https://www.linkedin.com/in/mateus-silva-893a4322b/',
+          icon: '/images/social_media_icons/twitter.svg',
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="mt-36">
       <h2 className="font-medium text-3xl md:text-4xl lg:text-5xl leading-tight mt-14 mb-16">
         {`Our Team`}
       </h2>
       <div className="grid grid-cols-3 grid-rows-2 gap-5">
-        <Card />
-        <Card />
-        <Card />
-        <div className="col-span-3 flex justify-center gap-5">
-          <Card />
-          <Card />
-        </div>
+        {members.map((member, index) => {
+          if (index === members.length - 2 && members.length % 2 !== 0) {
+            return (
+              <div className="col-span-3 flex justify-center gap-5">
+                <Card member={member} />
+                <Card member={members[index + 1]} />
+              </div>
+            );
+          } else if (index !== members.length - 1 || members.length % 2 === 0) {
+            return <Card member={member} />;
+          }
+        })}
       </div>
     </div>
   );
